b64cd98c692d990ccdc116386176b30fcdc37291
Jackson Myhre <jack421myhre@gmail.com>
2022-08-17 16:39:52 -0500

    Added the JS file to the project and added console interactivity

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+let topic = "HTML";
+let topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+// if (topic === "HTML") {
+//   console.log("Let's study HTML!");
+// } else if (topic === "CSS") {
+//   console.log("Let's study CSS!");
+// } else if (topic === "Git") {
+//   console.log("Let's study Git!");
+// } else if (topic === "JavaScript") {
+//   console.log("Let's study JavaScript");
+// } else {
+//   console.log("Please try again!");
+// }
+
+for (let i = 0; i < topics.length; i++) {
+  console.log(topics[i]);
+}
